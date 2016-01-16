193b3945b954c2aa42bff065640615cfb66ab3c8
Chuan Ji <jichu4n@gmail.com>
2016-01-16 11:49:58 -0800

    Support compiling in main thread also.

--- client/app/services/doc-manager.js
+++ client/app/services/doc-manager.js
@@ -2,6 +2,8 @@
  *                           Copyright 2016 Chuan Ji                         *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
+/* global asciidoctorJsCompile */
+
 import Ember from 'ember';
 
 export default Ember.Service.extend({
@@ -19,23 +21,37 @@ export default Ember.Service.extend({
       title: this.get('i18n').t('defaultTitle'),
       body: this.get('i18n').t('defaultBody')
     }));
-    this.set('compileWorker', new Worker(
-      '/assets/workers/asciidoctor-js-compile-worker.js'));
-    this.get('compileWorker').addEventListener(
-      'message', this.onCompileDone.bind(this));
+    if (window.Worker) {
+      this.set('compileWorker', new Worker(
+        '/assets/workers/asciidoctor-js-compile-worker.js'));
+      this.get('compileWorker').addEventListener(
+        'message', this.onCompileDone.bind(this));
+    } else {
+      console.log(
+        'Browser does not support Web Workers, will compile in main thread.');
+    }
   },
-  compile: Ember.observer('doc.body', function() {
+  compile: Ember.on('init', Ember.observer('doc.body', function() {
     if (Ember.isNone(this.get('doc'))) {
       return;
     }
     this.set('compileCount', this.get('compileCount') + 1);
     var body = this.get('doc.body').toString() || '';
-    this.get('compileWorker').postMessage({
+    var request = {
       body: body,
       compileCount: this.get('compileCount'),
       startTs: new Date()
-    });
-  }),
+    };
+    if (Ember.isNone(this.get('compileWorker'))) {
+      Ember.run.next(this, this.compileInMainThread, request);
+    } else {
+      this.get('compileWorker').postMessage(request);
+    }
+  })),
+  compileInMainThread(request) {
+    var response = asciidoctorJsCompile({ data: request });
+    this.onCompileDone({ data: response });
+  },
   onCompileDone(ev) {
     var endTs = new Date();
     console.info(
