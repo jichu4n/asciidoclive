ccdc9b542d7884c42c1d226292ddf93782922392
Chuan Ji <jichu4n@gmail.com>
2016-01-19 18:38:32 -0800

    Show storage type and full path in title.

--- client/app/application/route.js
+++ client/app/application/route.js
@@ -3,16 +3,24 @@
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 import Ember from 'ember';
+import StorageType from '../utils/storage-type';
 
 export default Ember.Route.extend({
   docManager: Ember.inject.service(),
+  i18n: Ember.inject.service(),
 
   titleToken() {
-    return this.get('docManager.doc.title');
+    var title = '';
+    if (this.get('docManager.doc.storageType') !== StorageType.NONE) {
+      title += this.get('i18n').t(
+        'storageType.' + this.get('docManager.doc.storageType'));
+    }
+    title += this.get('docManager.doc.title');
+    return title;
   },
   title(tokens) {
     console.info('Tokens: %o', tokens);
-    return tokens[0] + ' - AsciiDocLIVE';
+    return tokens[0] + this.get('i18n').t('titleSuffix');
   },
   updateTitle: Ember.observer('docManager.doc.title', function() {
     console.info('Updating title');
