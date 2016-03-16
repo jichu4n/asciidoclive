24d3e6b58e36facde343a1a744e16921fd24fcaa
Chuan Ji <jichu4n@gmail.com>
2016-03-16 14:09:44 -0700

    Implement synced scrolling.

--- client/app/utils/scroll-state.js
+++ client/app/utils/scroll-state.js
@@ -5,9 +5,9 @@
 import Ember from 'ember';
 
 export default Ember.Object.extend({
-  viewportHeight: null,
-  contentHeight: null,
-  scrollTop: null,
+  viewportHeight: 0,
+  contentHeight: 0,
+  scrollTop: 0,
 
   init() {
     // Force computation of scrollRatio to enable observers.
@@ -15,16 +15,19 @@ export default Ember.Object.extend({
   },
 
   maxScrollTop: Ember.computed('viewportHeight', 'contentHeight', function() {
-    return Math.max(0, this.get('contentHeight') - this.get('viewportHeight'));
+    return Math.max(
+      0, (this.get('contentHeight') - this.get('viewportHeight') || 0));
   }),
   scrollRatio: Ember.computed('scrollTop', 'maxScrollTop', {
     get() {
-      return this.get('maxScrollTop') > 0 ?
-        Math.max(0, Math.min(
-          1.0, this.get('scrollTop') / this.get('maxScrollTop'))) :
-        1.0;
+      if (this.get('maxScrollTop') > 0) {
+        var scrollRatio = this.get('scrollTop') / this.get('maxScrollTop');
+        return Math.max(0.0, Math.min(1.0, scrollRatio));
+      }
+      return 1.0;
     },
     set(key, value) {
+      value = Math.max(0, Math.min(1.0, value));
       this.set('scrollTop', value * this.get('maxScrollTop'));
       return value;
     }
