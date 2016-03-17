87dd561be6fc98de1ea9ede1b34e635311755e42
Chuan Ji <jichu4n@gmail.com>
2016-03-16 18:08:47 -0700

    Use ember-local-storage-proxy.

--- client/app/services/settings.js
+++ client/app/services/settings.js
@@ -3,31 +3,8 @@
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 import Ember from 'ember';
-
-function localStorageProperty(propertyName, defaultValue) {
-  return {
-    get() {
-      if (window.localStorage[propertyName] === undefined) {
-        window.localStorage[propertyName] = JSON.stringify(defaultValue);
-      }
-      return JSON.parse(window.localStorage[propertyName]);
-    },
-    set(key, value) {
-      window.localStorage[propertyName] = JSON.stringify(value);
-      return value;
-    }
-  };
-}
+import { localStorageProperty } from 'ember-local-storage-proxy';
 
 export default Ember.Service.extend({
-  init() {
-    if (Ember.isNone(window.localStorage) || !window.localStorage) {
-      console.warning('Browser does not support local storage!');
-      window.localStorage = {};
-    } else {
-      console.info('Browser supports local storage');
-    }
-  },
-
   syncScroll: Ember.computed(localStorageProperty('syncScroll', true))
 });
