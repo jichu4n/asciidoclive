b6044da280989f46d9f6519e350ed81bdc89a517
Chuan Ji <jichu4n@gmail.com>
2016-04-08 00:48:11 -0700

    Add Google Analytics

--- client/app/router.js
+++ client/app/router.js
@@ -4,8 +4,9 @@
 
 import Ember from 'ember';
 import config from './config/environment';
+import googlePageview from './mixins/google-pageview';
 
-const Router = Ember.Router.extend({
+const Router = Ember.Router.extend(googlePageview, {
   location: config.locationType
 });
 
