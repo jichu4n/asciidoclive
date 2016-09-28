a638592b7b71f4ab26b43e10fa4636680899204e
Chuan Ji <jichu4n@gmail.com>
2016-09-27 23:14:15 -0700

    Upgrade to Ember 2.8

--- client/app/app.js
+++ client/app/app.js
@@ -3,8 +3,8 @@
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 import Ember from 'ember';
-import Resolver from 'ember/resolver';
-import loadInitializers from 'ember/load-initializers';
+import Resolver from 'ember-resolver';
+import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
 let App;
