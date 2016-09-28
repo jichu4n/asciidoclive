54f023dbd8928573f08ef3c5b2770af388e4c743
Chuan Ji <jichu4n@gmail.com>
2016-09-27 23:20:52 -0700

    Remove baseURL and make URLs absolute

--- client/config/environment.js
+++ client/config/environment.js
@@ -4,7 +4,6 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'amoya',
     environment: environment,
-    baseURL: '/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
