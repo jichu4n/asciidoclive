0d7a07142e63b205c3fbc6f76969b13fe5c3000e
Chuan Ji <jichu4n@gmail.com>
2016-09-28 13:39:35 -0700

    Update test index.html

--- client/config/environment.js
+++ client/config/environment.js
@@ -47,7 +47,6 @@ module.exports = function(environment) {
 
   if (environment === 'test') {
     // Testem prefers this...
-    ENV.baseURL = '/';
     ENV.locationType = 'none';
 
     // keep test console output quieter
