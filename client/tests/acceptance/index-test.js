193749e51b375b8790850417925993cc42048e58
Chuan Ji <jichu4n@gmail.com>
2016-09-28 22:57:21 -0700

    Fix edit layout styling for testing; delay test completion for debugging

--- client/tests/acceptance/index-test.js
+++ client/tests/acceptance/index-test.js
@@ -8,9 +8,11 @@ import moduleForAcceptance from 'amoya/tests/helpers/module-for-acceptance';
 moduleForAcceptance('Acceptance | index');
 
 test('visiting /', function(assert) {
+  var done = assert.async();
   visit('/');
 
   andThen(function() {
     assert.equal(currentURL(), '/edit/scratch/1');
+    setTimeout(done, 30000);
   });
 });
