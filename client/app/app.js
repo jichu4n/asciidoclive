4fa081042891106b0c029696f77f9f3aa8931e98
Chuan Ji <ji@chu4n.com>
2018-06-13 22:55:12 -0700

    Commit existing changes.

--- client/app/app.js
+++ client/app/app.js
@@ -1,9 +1,5 @@
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *                           Copyright 2016 Chuan Ji                         *
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-
 import Ember from 'ember';
-import Resolver from 'ember-resolver';
+import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
 
