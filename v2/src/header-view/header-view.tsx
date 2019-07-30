f920657efe544c46a635f5a1877e2fdd06dd3dc7
Chuan Ji <chuan@jichu4n.com>
2019-07-29 22:49:09 -0700

    App logo.
    Created in Inkscape with Raleway font.

--- v2/src/header-view/header-view.tsx
+++ v2/src/header-view/header-view.tsx
@@ -1,6 +1,5 @@
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import * as React from 'react';
 
 class HeaderView extends React.Component {
@@ -8,9 +7,7 @@ class HeaderView extends React.Component {
     return (
       <AppBar position="fixed">
         <Toolbar>
-          <Typography variant="h6" noWrap={true}>
-            asciidoclive
-          </Typography>
+          <img src="/assets/logo.svg" className="header-logo" />
         </Toolbar>
       </AppBar>
     );
