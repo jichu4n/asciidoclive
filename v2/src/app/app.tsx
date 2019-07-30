6bae5abb1afd14c08b8c62ac145e177350290bc1
Chuan Ji <chuan@jichu4n.com>
2019-07-29 18:14:22 -0700

    Material-UI theme.

--- v2/src/app/app.tsx
+++ v2/src/app/app.tsx
@@ -1,15 +1,24 @@
+import {blueGrey as primaryColor} from '@material-ui/core/colors';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import {createMuiTheme} from '@material-ui/core/styles';
+import ThemeProvider from '@material-ui/styles/ThemeProvider';
 import * as React from 'react';
 import EditView from '../edit-view/edit-view';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import './app.css';
+
+const THEME = createMuiTheme({
+  palette: {
+    primary: primaryColor,
+  },
+});
 
 class App extends React.Component {
   public render() {
     return (
-      <>
+      <ThemeProvider theme={THEME}>
         <CssBaseline />
         <EditView />;
-      </>
+      </ThemeProvider>
     );
   }
 }
